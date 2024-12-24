dd9e217d7eed853f66667335af6d1d45020b4b98
manish mandal <76776194mm@gmail.com>
2024-12-25 00:35:39 +0530

    configured routes and controller for contact creation

--- server/app.js
+++ server/app.js
@@ -1,5 +1,12 @@
 const express = require('express');
 const app = express();
 
+const contactRoute = require('./routes/contactRoute');
+
+app.use(express.json());
+
+app.use('/api/contacts', contactRoute);
+
+
 module.exports = app;
 
